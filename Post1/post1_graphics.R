7ce62ebe8e44d6579b76ce837496e9f167f4a215
Vinícius Obadowski <vobadowski@gmail.com>
2018-03-31 11:01:24 -0300

    Inclusão do Arquivo do primeiro post. Apenas ajuste de bibliotecas, cabeçalhos e tal.

---
+++ Post1/post1_graphics.R
@@ -0,0 +1,12 @@
+##### Post 1 - Desenvolvimento Mundial
+## Feito por Obadowski
+
+# Carregando bibliotecas
+library(tidyverse)
+library(ggthemes)
+library(ggrepel)
+library(dslabs)
+
+# Obtendo base de dados da Gapminder
+data(gapminder)
+
